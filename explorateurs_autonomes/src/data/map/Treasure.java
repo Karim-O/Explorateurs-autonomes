16d3d4fb144d326d61f942af9a7d274eacd74720
PC <PC@DESKTOP-SID4RRJ>
2023-02-12 01:19:20 +0100

    nouvelle classe treasure

---
+++ explorateurs_autonomes/src/data/map/Treasure.java
@@ -0,0 +1,28 @@
+package data.map;
+
+import java.awt.Dimension;
+
+import data.map.geometry.Position;
+
+
+public class Treasure extends GraphicElement{
+
+	private int loot;
+	
+	private static final int DEFAULT_LOOT = 10000;
+	
+	public Treasure(Dimension dimension, Position position, int loot) {
+		super(dimension, position);
+		this.loot = loot;
+	}
+	
+	public Treasure(Dimension dimension, Position position) {
+		this(dimension, position, DEFAULT_LOOT);
+	}
+
+	public int getLoot() {
+		return loot;
+	}
+	
+	
+}
