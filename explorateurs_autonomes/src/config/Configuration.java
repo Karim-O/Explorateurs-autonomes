fd3407778c9a902799d6239bd4ff4d036f462037
PC <PC@DESKTOP-SID4RRJ>
2023-02-12 16:51:27 +0100

    Classe monster and classe Config

--- explorateurs_autonomes/src/config/Configuration.java
+++ explorateurs_autonomes/src/config/Configuration.java
@@ -1,5 +1,19 @@
 package config;
 
+/**
+ * Configuration
+ * 
+ * @author Feriel MALEK
+ *
+ */
 public class Configuration {
+	public static final int WINDOW_WIDTH = 400;
+	public static final int WINDOW_HEIGHT = 500;
+	public static final String TITLE = "GUI";
+	
+	public static final int GAME_SPEED = 1000;
+	
+	
+	
 
 }
--- explorateurs_autonomes/src/data/map/mobile/Monster.java
+++ explorateurs_autonomes/src/data/map/mobile/Monster.java
@@ -1,5 +1,24 @@
 package data.map.mobile;
 
-public class Monster {
+import java.awt.Dimension;
+
+import data.map.geometry.CircularSector;
+import data.map.geometry.Position;
+
+/**
+ * This class represents a character of monster
+ * 
+ * @version 1.0
+ * @author Feriel MALEK
+ * */
+
+
+public class Monster extends MobileElement {
+
+	public Monster(Position position, Dimension dimension, float pace, String name, int strength, CircularSector vision,
+			Sound sound) {
+		super(position, dimension, pace, name, strength, vision, sound);
+		
+	}
 
 }
