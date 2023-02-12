8ba534004fbb140deeb1c3a6c00dfbf5a8fbd720
omar <omar@omar-Latitude-3520>
2023-02-12 14:24:27 +0100

    Treasure

--- explorateurs_autonomes/src/data/map/Treasure.java
+++ explorateurs_autonomes/src/data/map/Treasure.java
@@ -1,7 +1,29 @@
 package data.map;
 
-public class Treasure {
-	
-	public Treasure() {}
+import java.awt.Dimension;
+
+import data.map.geometry.Position;
+
+
+public class Treasure extends GraphicElement{
+
+    private int loot;
+
+    private static final int DEFAULT_LOOT = 10000;
+
+    public Treasure(Dimension dimension, Position position, int loot) {
+        super(dimension, position);
+        this.loot = loot;
+    }
+
+    public Treasure(Dimension dimension, Position position) {
+        this(dimension, position, DEFAULT_LOOT);
+    }
+
+    public int getLoot() {
+
+        return loot;
+    }
+
 
 }
