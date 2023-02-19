a98205dac854d35e6f7f85a3977255ac9ca2e1ed
omar <omar@omar-Latitude-3520>
2023-02-19 20:46:38 +0100

    Revert "Classe welcomeGUI"
    This reverts commit 17317ca15dc2f76945b275c4fd0250c31422796a.

--- explorateurs_autonomes/src/config/Configuration.java
+++ explorateurs_autonomes/src/config/Configuration.java
@@ -51,45 +51,6 @@ public class Configuration {
 	/** Game speed in ms **/
 	public static final int GAME_SPEED = 1000;
 	
-	public static final int WINDOW_WIDTH_WELCOME = 600;
-	public static final int WINDOW_HEIGHT_WELCOME = 400;
-
-	
-	/** Width of the description in the welcome frame **/
-	public static final int DESCRIPTION_WELCOME_WIDTH = 300;
-	
-	/** Height of the description in the welcome frame **/
-	public static final int DESCRIPTION_WELCOME_HEIGHT = 110;
-	
-	/** Position X of the description in the welcome frame **/
-	public static final int DESCRIPTION_WELCOME_POSITION_X = (WINDOW_WIDTH_WELCOME/2) - (DESCRIPTION_WELCOME_WIDTH/2);
-	/** Position Y of the description in the welcome frame **/
-	public static final int DESCRIPTION_WELCOME_POSITION_Y = (WINDOW_HEIGHT_WELCOME/2) - (DESCRIPTION_WELCOME_HEIGHT/2);
-	
-	
-
-	/** Width of the start button in the welcome frame **/
-	public static final int START_WELCOME_WIDTH = 200;
-	
-	/** Height of the start button in the welcome frame **/
-	public static final int START_WELCOME_HEIGHT = 60;
-	
-	/** Position X of the start BUTTON in the welcome frame **/
-	public static final int START_WELCOME_POSITION_X = (WINDOW_WIDTH_WELCOME/2) - (START_WELCOME_WIDTH/2);
-	/** Position Y of the start button in the welcome frame **/
-	public static final int START_WELCOME_POSITION_Y = (WINDOW_HEIGHT_WELCOME/2) - (START_WELCOME_HEIGHT/2) + (DESCRIPTION_WELCOME_HEIGHT);
-	
-	
-	/** Width of the title picture in the welcome frame **/
-	public static final int TITLE_WELCOME_WIDTH = 400;
-	
-	/** Height of the title picture in the welcome frame **/
-	public static final int TITLE_WELCOME_HEIGHT = 90;
-	
-	/** Position X of the title picture in the welcome frame **/
-	public static final int TITLE_WELCOME_POSITION_X = (WINDOW_WIDTH_WELCOME/2) - (TITLE_WELCOME_WIDTH/2);
-	/** Position Y of the title picture in the welcome frame **/
-	public static final int TITLE_WELCOME_POSITION_Y = (WINDOW_HEIGHT_WELCOME/2) - (TITLE_WELCOME_HEIGHT/2) - (DESCRIPTION_WELCOME_HEIGHT);
 	
 	
 
